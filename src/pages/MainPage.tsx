f27a6a5bb1a77a24ec0aced9b5ff6ef95f6d6bba
uttne <uttne.mkr@gmail.com>
2025-02-13 23:46:55 +0900

    add mainpage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,8 +3,11 @@ import { Stack, Card } from "@mui/joy";
 export function MainPage() {
   return (
     <>
-      <Stack spacing={1}>
-        <Card></Card>
+      <Stack spacing={1} direction="row" useFlexGap sx={{flexWrap: "wrap"}}>
+        <Card>aaaa</Card>
+        <Card>aaaa</Card>
+        <Card>aaaa</Card>
+        <Card>aaaa</Card>
       </Stack>
     </>
   );
