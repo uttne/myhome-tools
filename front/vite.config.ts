5eed7b05dcc9b814f71bc40b72ea168304c8abe6
uttne <uttne.mkr@gmail.com>
2025-03-28 22:04:51 +0900

    tailwindcss を導入

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
         // 127.0.0.1 で起動している場合 localhost では接続できない
         target: "http://127.0.0.1:8888/",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
