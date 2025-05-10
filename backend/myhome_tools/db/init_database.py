2a24b45b32f461980e9b433009c9bfccdce4b246
uttne <uttne.mkr@gmail.com>
2025-05-11 02:16:50 +0900

    コメントを追加

--- backend/myhome_tools/db/init_database.py
+++ backend/myhome_tools/db/init_database.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
 
-from typing import Iterable, Optional, List
+from typing import Iterable
 from sqlalchemy import Table
 from sqlmodel import SQLModel
 from pathlib import Path
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
 from myhome_tools.db.engine import attach_dbs_async
 
 def tables_for(schema: str) -> Iterable["Table"]:
+    """
+    指定されたスキーマに属するテーブルを取得する
+    """
     return [
         tbl for tbl in SQLModel.metadata.sorted_tables if tbl.schema == schema
     ]
 
 async def init_db(session:AsyncSession, db_path: Path, schema: str) -> None:
+    """
+    指定されたスキーマのデータベースを初期化する
+    """
 
     if db_path.exists():
         print(f"Database already exists at {db_path}")
