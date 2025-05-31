ea08d88737dda6b101e821fdc47e8aaf0c99ac73
uttne <uttne.mkr@gmail.com>
2025-06-01 03:17:37 +0900

    パラメータが無いようにラッパーを作成

--- backend/myhome_tools/api/depends/type.py
+++ backend/myhome_tools/api/depends/type.py
@@ -1,6 +1,7 @@
-from typing import Annotated
+from typing import Annotated, AsyncIterator
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
 from myhome_tools.api.depends.jwt import get_sub_from_verified_token
 from myhome_tools.db.engine import get_async_session
@@ -8,7 +9,14 @@ from myhome_tools.db.engine import get_async_session
 # ════════════════════════════════════════════════════════════════
 # FastAPI の依存性注入用
 # ════════════════════════════════════════════════════════════════
-DbSessionDep = Annotated[AsyncSession, Depends(get_async_session)]
+
+@asynccontextmanager
+async def get_async_session_no_params() -> AsyncIterator[AsyncSession]:
+    """引数が存在すると API から指定ができてしまうので、引数なしの関数を定義する"""
+    async with get_async_session() as session:
+        yield session
+
+DbSessionDep = Annotated[AsyncIterator[AsyncSession], Depends(get_async_session_no_params)]
 """
 # FastAPI で DB を使う用
 
