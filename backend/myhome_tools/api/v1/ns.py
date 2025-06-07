1bff836011f4c3ed089681620877078dcb64c20b
uttne <uttne.mkr@gmail.com>
2025-06-07 11:39:21 +0900

    namespace を共有するAPIを追加

--- backend/myhome_tools/api/v1/ns.py
+++ backend/myhome_tools/api/v1/ns.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, ConfigDict
-from sqlmodel import or_, and_, select
+from sqlmodel import or_, and_, select, col
+from sqlalchemy import func
 
 from myhome_tools.api.depends.type import DbSessionDep, SubDep
 from myhome_tools.api.utils.funcs import create_id
@@ -133,13 +134,132 @@ async def create_namespace(
 
     return namespace
 
+
+class AddNamespaceUserRequest(BaseModel):
+    """
+    Namespace にユーザを追加するためのリクエストボディ
+    - user_id: 追加するユーザの ID
+    - role: ユーザの役割 (read, write, admin)
+    """
+    user_id: str
+    role: NamespaceUserRole
+
+    model_config = ConfigDict(from_attributes=True)
+
 @router.post("/api/v1/ns/{namespace_id}/users")
-async def add_user():
-    return {"msg": "Hello World"}
+async def add_user(
+    db: DbSessionDep,
+    sub: SubDep,
+    request_in: list[AddNamespaceUserRequest],
+    namespace_id: str
+):
+    
+    async with db as session:
+        user = await get_me(session, sub, settings)  # ユーザ情報を取得しておく
+        async with attach_dbs_async(session, {
+                A_APP: DB_APP,
+            }) as ses:
+
+            sl = await ses.exec(
+                select(AppNamespace)
+                .where(
+                    AppNamespace.id == namespace_id,
+                    AppNamespaceUser.user_id == user.id,
+                    or_(
+                        AppNamespaceUser.role == NamespaceUserRole.admin,
+                        AppNamespaceUser.role == NamespaceUserRole.write
+                    )
+                )
+            )
+            namespace = sl.first()
+
+            if namespace is None:
+                raise HTTPException(status_code=404, detail="Namespace not found")
+            
+            # Chekc existing users
+            sl = await ses.exec(
+                select(func.count(AppUser.id)).where(
+                    col(AppUser.id).in_([r.user_id for r in request_in])
+                )
+            )
+            existing_users_count = sl.one()
+            if existing_users_count != len(request_in):
+                raise HTTPException(status_code=404, detail="One or more users not found")
+
+
+            sl = await ses.exec(
+                select(AppNamespaceUser).where(
+                    and_(
+                        AppNamespaceUser.namespace_id == namespace_id,
+                    )
+                )
+            )
+
+            users = sl.all()
+
+            user_set = set(u.user_id for u in users)
+
+            add_users = [
+                r for r in request_in if r.user_id not in user_set
+            ]
+            update_users = [
+                r for r in request_in if r.user_id in user_set
+            ]
+
+            ses.add_all([
+                AppNamespaceUser(
+                    id=create_id(),
+                    namespace_id=namespace.id,
+                    user_id=r.user_id,
+                    role=r.role,
+                )
+                for r in add_users
+            ])
+
+            for r in update_users:
+                t = next(iter(u for u in users if u.user_id == r.user_id),None)
+                if t is not None:
+                    t.role = r.role
+            
+            await ses.commit()
+    
+    return {"msg": "Users added or updated successfully"}
+
+
+class ApiResNamespaceUser(BaseModel):
+    id: str
+    sub: str
+    name: str
+    namespace_id: str
+    role: NamespaceUserRole
+
+    model_config = ConfigDict(from_attributes=True)
 
 @router.get("/api/v1/ns/{namespace_id}/users")
-async def get_namespace_users():
-    return {"msg": "Hello World"}
+async def get_namespace_users(
+    db: DbSessionDep,
+    sub: SubDep,
+    namespace_id: str
+):
+    async with db as session:
+        user = await get_me(session, sub, settings)  # ユーザ情報を取得しておく
+        async with attach_dbs_async(session, {
+                A_APP: DB_APP,
+            }) as ses:
+
+            sl = await ses.exec(
+                select(AppUser,AppNamespaceUser)
+                .where(
+                    AppNamespaceUser.namespace_id == namespace_id,
+                    AppNamespaceUser.user_id == user.id
+                )
+            )
+            users = sl.all()
+
+            if user.id not in [u.id for u, _ in users]:
+                raise HTTPException(status_code=404, detail="Namespace not found or you do not have access to it")
+            
+    return [ApiResNamespaceUser.model_validate({ **ns_usr.model_dump(), **u.model_dump(),}) for u, ns_usr in users]
 
 @router.delete("/api/v1/ns/{namespace_id}/users/{user_id}")
 async def remove_namespace_user():
